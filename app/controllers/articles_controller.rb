0c1ea1f7cd57e469122b16eeb9c293a68d71cc25
eoneon <eoneon123@gmail.com>
2016-02-16 22:15:39 +0000

    Add partials and destroy action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,9 @@
 class ArticlesController < ApplicationController
+
   def index
     @articles = Article.all
   end
-  
+
   def new
     @article = Article.new
   end
@@ -31,9 +32,18 @@ class ArticlesController < ApplicationController
       render 'edit'
     end
   end
+
   def show
     @article = Article.find(params[:id])
   end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article was successfuly deleted"
+    redirect_to articles_path
+  end
+
   private
     def article_params
       params.require(:article).permit(:title, :description)
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'welcome#home'
-  get 'welcome/about', to: 'welcome#about'
+  root 'pages#home'
+  get 'pages/about', to: 'pages#about'
   resources :articles
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
